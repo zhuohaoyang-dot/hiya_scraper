9f1300dd4b165050cdcba539171308ee77a9b5c3
00ikaros <zhuohao@uchicago.edu>
2025-10-15 14:18:54 -0500

    configure

--- api.py
+++ api.py
@@ -45,4 +45,6 @@ def scrape_hiya():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    import os
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
